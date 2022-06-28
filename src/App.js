e7489319f1ca11e1efcd31cb3acde938546b976f
Usman Khalid <65516140+Rajputusman@users.noreply.github.com>
2022-06-28 11:42:06 +0500

    React.js, React Hooks, Redux - Building A React App 3
    this don't have access to parent component use class and arrow function
    with function or while declaring the function in the element by using
    callback function ().

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CardList from "./CardList";
+import {robots} from './robots';
+import SearchBox from './SearchBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            searchField: '',
+            robots: robots
+        }
+    }
+
+    onSearchChange = (e) => {
+        this.setState({searchField:e.target.value});
+        
+    }
+
+    render(){
+
+        const filterRobots =  this.state.robots.filter(robot => 
+            robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()
+            ));  
+
+    return(
+
+        <div className="tc">
+            <h1>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange} />
+            <CardList robots={filterRobots}/>
+        </div>
+        
+    );
+    }
 }
 
 export default App;
