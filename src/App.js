729ead6b02fec993405a06c2f8a05372ba8c9aee
Usman Khalid <65516140+Rajputusman@users.noreply.github.com>
2022-06-28 13:28:25 +0500

    React.js, React Hooks, Redux - Building A React App 5Building A React App 5 Amazing Stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import {robots} from './robots';
+// import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -11,8 +11,17 @@ class App extends Component {
         super()
         this.state = {
             searchField: '',
-            robots: robots
+            robots: []
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+            return response.json();
+        }).then(users => {
+            
+            // this.setState({robots:users})
+        })
     }
 
     onSearchChange = (e) => {
@@ -26,15 +35,22 @@ class App extends Component {
             robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()
             ));  
 
-    return(
-
-        <div className="tc">
-            <h1 className="f1">RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
-            <CardList robots={filterRobots}/>
-        </div>
+    if(this.state.robots.length === 0){
         
-    );
+        return <h1 className="tc f1 flex justify-center items-center vh-100"> Loading </h1>;
+    } else{
+        return(
+
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filterRobots}/>
+            </div>
+            
+        );
+    }       
+
+    
     }
 }
 
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({searchChange}) => {
   return(
     <div className="pa2">
         <input 
-        className="pa3 ba b--green bg-lightest-blue"
+        className="pa3 ba b--green bg-lightest-blue outline-0"
         type="search" 
         name="" 
         onChange={searchChange} 
